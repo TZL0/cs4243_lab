19aa83fc0ef1442f066d0bb09e8ada0be4bb371f
TZL0 <tianzeee@outlook.com>
2023-10-17 17:26:04 +0800

    Finish cw2 q5

---
+++ Python_notebooks/set5/cs2q5.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+
+# Load images
+frame1 = cv2.imread("/Users/tianze/cs4243_lab/CS4243_2023_images_small/fr11.png", 0)
+frame2 = cv2.imread("/Users/tianze/cs4243_lab/CS4243_2023_images_small/fr12.png", 0)
+
+x_sobel_kernel = np.array([[-1 , 0 , 1] , [-2 , 0 , 2] , [-1 , 0 , 1]])
+y_sobel_kernel = np.array([[-1 , -2 , -1] , [0 , 0 , 0] , [1 , 2 , 1]])
+
+sobel_x1 = cv2.filter2D(src=frame1, ddepth=-1, kernel=x_sobel_kernel)
+sobel_x2 = cv2.filter2D(src=frame2, ddepth=-1, kernel=x_sobel_kernel)
+
+
+y1, x1 = np.unravel_index(np.argmax(sobel_x1), sobel_x1.shape)
+y2, x2 = np.unravel_index(np.argmax(sobel_x2), sobel_x2.shape)
+
+# Display the frames with the strongest vertical edges
+cv2.namedWindow("fr11", cv2.WINDOW_NORMAL)
+cv2.imshow("fr11", sobel_x1)
+cv2.waitKey(0)
+cv2.imshow("fr12", sobel_x2)
+cv2.waitKey(0)
+merged = cv2.addWeighted(sobel_x1, 0.5, sobel_x2, 0.5, 0)
+cv2.line(merged, (x1, y1), (x2, y2), (255, 255, 255), 2)
+cv2.imshow("fr11+fr12", merged)
+cv2.waitKey(0)
+
+# Calculate pixel displacement in y-direction
+d_pixels = ((y2 - y1) ** 2 + (x2 - x1) ** 2) ** 0.5
+
+d_meters = d_pixels * 0.05
+v = d_meters / 2.5
+
+print("Displacement:", d_meters, 'm')
+print("Velocity:", v, "m/s")
+
+# Real-world applications of this algorithm:
+# j. This algorithm can be used in scenarios like traffic monitoring to detect the speed of vehicles, in sports analytics to compute the speed of a ball or player, or in security applications to detect suspicious movements.
+# k. The real-world applications include video surveillance systems to detect fast-moving objects, autonomous vehicles for obstacle detection and velocity estimation, robotics for motion planning, and in augmented reality to understand and respond to real-world movements.
---
+++ Python_notebooks/set5/optical_flow.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+
+# The video feed is read in as
+# a VideoCapture object
+# cap = cv2.VideoCapture("/Users/tianze/cs4243_lab/CS4243_2023_images_small/10236.mp4")
+
+old_frame = cv2.imread("/Users/tianze/cs4243_lab/CS4243_2023_images_small/fr11.png")
+frame = cv2.imread("/Users/tianze/cs4243_lab/CS4243_2023_images_small/fr12.png")
+# ret, old_frame = cap.read()
+old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+p0 = cv2.goodFeaturesToTrack(old_gray, maxCorners=100, qualityLevel=0.3, minDistance=7)
+
+frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+# Calculate optical flow
+lk_params = dict(winSize=(15, 15), maxLevel=2, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+p1, st, err = cv2.calcOpticalFlowPyrLK(
+    old_gray, frame_gray, p0, None, **lk_params
+)
+
+# Select good points
+good_new = p1[st == 1]
+good_old = p0[st == 1]
+
+# Draw optical flow vectors
+for i, (new, old) in enumerate(zip(good_new, good_old)):
+    a, b = new.ravel()
+    c, d = old.ravel()
+    cv2.line(frame, (int(a), int(b)), (int(c), int(d)), (0, 255, 0), 2)
+    cv2.circle(frame, (int(a), int(b)), 5, (0, 0, 255), -1)
+
+# Display the frame with optical flow vectorsqq
+cv2.namedWindow("Optical Flow", cv2.WINDOW_NORMAL)
+cv2.imshow("Optical Flow", frame)
+# Update the previous frame and points
+old_gray = frame_gray.copy()
+p0 = good_new.reshape(-1, 1, 2)
+
+if cv2.waitKey(0) & 0xFF == ord("q"):
+    pass
+
+# Release the video capture and close the display window
+cv2.destroyAllWindows()
